4916de1b4227978e506df0ec79bc2c0ce7c7e495
FathullaLatipov <fathullaxonlatipov@gmail.com>
2024-02-01 16:13:05 +0500

    docker compose add

--- redis_service/__init__.py
+++ redis_service/__init__.py
@@ -1,11 +1,11 @@
-# import redis
-#
-# # Создать подключения к редису
-# redis_db = redis.from_url('redis://redis_db')
-#
-# # Создать запись в базу редис
-# redis_db.set("spam", 10) # {"spam": 10}
-#
-# # Получить значения из базы
-# data = redis_db.get("spam")
-# print(data)
+import redis
+
+# Создать подключения к редису
+redis_db = redis.from_url('redis://redis_db')
+
+# Создать запись в базу редис
+redis_db.set("spam", 10) # {"spam": 10}
+
+# Получить значения из базы
+data = redis_db.get("spam")
+print(data)
